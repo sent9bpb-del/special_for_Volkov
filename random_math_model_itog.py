ad8990531742f8aae2aad7d653845bc778563bfb
*kvazar* <*sent9bpb@gmail.com*>
2025-12-14 17:19:37 +0300

    input data and consts

--- random_math_model_itog.py
+++ random_math_model_itog.py
@@ -1,3 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from random_process import z
+
+R = 8.31
+A1 = 50
+E1 = 64000
+A2 = 10
+E2 = 44000
+A3 = 10**6
+E3 = 108000
+A4 = 25 * 10**5
+E4 = 112000
+
+m_c1_vh = z
+m_vozd_vh = 0.28
+
+ro = 1.6
+
+mu_ch3oh = 0.032
+mu_o2 = 0.032
+mu_hcho = 0.030
+mu_h2 = 0.002
