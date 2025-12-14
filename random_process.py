0cb6e65b348edacf2f2cf69477f2fcabd9c90f5c
*kvazar* <*sent9bpb@gmail.com*>
2025-12-14 17:12:00 +0300

    mport and input data

---
+++ random_process.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+lambda1 = math.pow(5, 9)
+lambda2 = math.pow(3, 8)
+
+M0 = 0.28
+sigma0_2 = 26 * 10**(-4)
+alpha0 = 0.09
+
+N = 210
+Kz_num = 20
+Z0 = 1
+
+A1 = 1
+A2 = 1
+Ns = 10
