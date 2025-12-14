38cd39303558a4ea6ac6b58e2c9c849747b57bd6
*kvazar* <*sent9bpb@gmail.com*>
2025-12-14 17:15:01 +0300

    symplex method

--- random_process.py
+++ random_process.py
@@ -69,4 +69,49 @@ def generate_process_z(x, A1, A2, sigma0_2, sigma_x0_2, alpha0, M0, Ns):
         z.append(ssum / Ns + M0)
     return z
 
+def simplex_search(x, sigma_x2, M0, sigma0_2, alpha0, Ns,
+                   A1_init=1.0, A2_init=1.0,
+                   tol=0.1, max_iter=200):
+
+    def process_error(A1, A2):
+        if A1 <= 0 or A2 <= 0:
+            return 1e6
+        z = generate_process_z(x, A1, A2, sigma0_2, sigma_x2, alpha0, M0, Ns)
+        Mz = checkmate_waiting(z)
+        sigma_z2 = dispersion(z, Mz)
+        Kz = correlation_function(z, Mz, Kz_num)
+        alpha_z, _ = approximate_alpha(range(Kz_num), Kz, sigma_z2)
+        return (
+            abs((Mz - M0) / M0)
+            + abs((sigma_z2 - sigma0_2) / sigma0_2)
+            + abs((alpha_z - alpha0) / alpha0)
+        )
+
+    simplex = [
+        np.array([A1_init, A2_init]),
+        np.array([A1_init + 0.2, A2_init]),
+        np.array([A1_init, A2_init + 0.2])
+    ]
+
+    values = [process_error(p[0], p[1]) for p in simplex]
+
+    for _ in range(max_iter):
+        order = np.argsort(values)
+        simplex = [simplex[i] for i in order]
+        values = [values[i] for i in order]
+
+        best, worst, second_worst = simplex[0], simplex[-1], simplex[-2]
+        centroid = (best + second_worst) / 2
+
+        reflected = centroid + (centroid - worst)
+        fr = process_error(reflected[0], reflected[1])
+
+        if fr < values[-1]:
+            simplex[-1], values[-1] = reflected, fr
+
+        if np.std(values) < tol:
+            break
+
+    return simplex[0][0], simplex[0][1]
+
 
