3966a4a55fd6fc636765ac99a3b98478b2922771
*kvazar* <*sent9bpb@gmail.com*>
2025-12-14 17:12:27 +0300

    congurent generator

--- random_process.py
+++ random_process.py
@@ -16,3 +16,9 @@ Z0 = 1
 A1 = 1
 A2 = 1
 Ns = 10
+
+def generation_congurent_method(n, lam1, lam2, x0):
+    x = [x0]
+    for i in range(1, n):
+        x.append((lam1 * x[i - 1]) % lam2)
+    return [xi / lam2 - 0.5 for xi in x]
